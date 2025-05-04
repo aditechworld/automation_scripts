4cd52b4c89f3546132f00fe6b1c5db93bf1efb45
Aditya Kumar <aditechworld2@gmail.com>
2025-05-04 15:42:44 +0530

    fixed skip empty message contact

--- whatsapp.py
+++ whatsapp.py
@@ -51,37 +51,20 @@ driver = webdriver.Chrome(options=chrome_options)
 
 
 for number,message in get_contact_message_pairs(df):
-    if len(str(message).trim()) == 0:
+    if len(str(message).strip()) == 0:
         continue
     encoded_msg = urllib.parse.quote(message)
     wa_url = f"https://web.whatsapp.com/send?phone={number}&text={encoded_msg}"
     driver.get(wa_url)
 
-    try:
-        # Click "Continue to Chat"
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "action-button"))
-        ).click()
-    except:
-        print(f"No continue button for {number}.")
-
-    try:
-        # Click "Use WhatsApp Web"
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, "use WhatsApp Web"))
-        ).click()
-    except:
-        print(f"No WhatsApp Web button for {number}.")
 
     try:
         send_btn = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Send"]'))
-)
+            EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Send"]')))
         send_btn.click()
         print(f"Message sent to {number}")
     except Exception as e:
         print(f"Failed to send message to {number}")
-        print(f"expection: {e}")
 
     time.sleep(1)  # wait between contacts
 
